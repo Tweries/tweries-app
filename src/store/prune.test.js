82c0757545e52b89ca6a96d8fdd1381cc3880a44
mattia richetto <mattia.richetto@gmail.com>
2019-10-19 00:21:48 -0700

    moar prune test

--- src/store/prune.test.js
+++ src/store/prune.test.js
@@ -1,9 +1,19 @@
 import v from 'voca';
 
+const SOURCE = 'YATTA, ';
+
+test('prune 4', () => {
+  expect(v.prune(SOURCE, 4, '')).toEqual('');
+});
+
 test('prune 5', () => {
-  expect(v.prune('YATTA,', 5, '')).toEqual('YATTA');
+  expect(v.prune(SOURCE, 5, '')).toEqual('YATTA');
 });
 
 test('prune 6', () => {
-  expect(v.prune('YATTA,', 6, '')).toEqual('YATTA,');
+  expect(v.prune(SOURCE, 6, '')).toEqual('YATTA'); // INFO: this should be 'YATTA,' to me
+});
+
+test('prune 7', () => {
+  expect(v.prune(SOURCE, 7, '')).toEqual('YATTA, ');
 });
