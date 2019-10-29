fab54e9909108fb98fda34f24e99a86687ce0186
mattia richetto <mattia.richetto@gmail.com>
2019-10-29 15:55:46 -0700

    add link to twitter

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,14 @@ import './Footer.css';
 function Footer({ healthy, version }) {
   return (
     <footer className={classnames({ healthy: healthy })}>
-      Tweries v{version} &copy; {new Date().getFullYear()}
+      Tweries v{version} &copy; {new Date().getFullYear()}{' '}
+      <a
+        href="https://twitter.com/TweriesApp"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        @TweriesApp
+      </a>
     </footer>
   );
 }
