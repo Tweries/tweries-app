aa126d78536d00279910e96335679ecd15257d6d
mattia richetto <mattia.richetto@gmail.com>
2019-10-30 17:34:04 -0700

    use new api

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://china-musk-api.herokuapp.com'; // 'https://china-musk-api.herokuapp.com' | 'http://localhost:9000'
+export const BASE_URL = 'https://tweries-api.herokuapp.com'; // 'https://tweries-api.herokuapp.com' | 'http://localhost:9000'
 
 export const FEATURE_V1 = 'FEATURE_V1';
 export const SPACE_AFTER_PUNCTUATION_V1 = 'SPACE_AFTER_PUNCTUATION_V1';
