6b09412c61843b347ebae87237e3a91241a5505e
mattia richetto <mattia.richetto@gmail.com>
2019-10-30 23:10:09 -0700

    check BASE_URL based on NODE_ENV

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,9 @@
-export const BASE_URL = 'https://tweries-api.herokuapp.com'; // 'https://tweries-api.herokuapp.com' | 'http://localhost:9000'
+const { NODE_ENV } = process.env;
+
+export const BASE_URL =
+  NODE_ENV === 'development'
+    ? 'http://localhost:9000'
+    : 'https://tweries-api.herokuapp.com';
 
 export const FEATURE_V1 = 'FEATURE_V1';
 export const SPACE_AFTER_PUNCTUATION_V1 = 'SPACE_AFTER_PUNCTUATION_V1';
