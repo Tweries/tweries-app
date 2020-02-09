874a1eee8fa50dfc2b7b5d7bcc28b0f4487069aa
Mattia Richetto <mattia.richetto@gmail.com>
2020-02-09 15:12:41 -0800

    cleanup

--- src/containers/App/TweetstormButton.js
+++ src/containers/App/TweetstormButton.js
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import classnames from 'classnames';
-import React from 'react';
+import React from 'react'; // TODO: move to components
 
 function TweetstormButton({ disabled, onClick, waiting }) {
   return (
