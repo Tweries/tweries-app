9d46577e88acd46dad5821db60ba5d8120b439f3
mattia richetto <mattia.richetto@gmail.com>
2020-04-02 20:06:23 -0700

    toggle on

--- src/features.js
+++ src/features.js
@@ -1,5 +1,10 @@
-import { FEATURE_V1, HIDE_TAGS_V1, SHOW_INFO_V1 } from './constants';
+import {
+  ADD_IMAGE_V1,
+  FEATURE_V1,
+  HIDE_TAGS_V1,
+  SHOW_INFO_V1
+} from './constants';
 
-const features = [FEATURE_V1, HIDE_TAGS_V1, SHOW_INFO_V1];
+const features = [ADD_IMAGE_V1, FEATURE_V1, HIDE_TAGS_V1, SHOW_INFO_V1];
 
 export default features;
