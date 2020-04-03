4fec99a913b2578ed132c1356354d81acb221111
mattia richetto <mattia.richetto@gmail.com>
2020-04-02 18:58:52 -0700

    add ADD_IMAGE_V1 toggle

--- src/api/fetchTweetstorm.js
+++ src/api/fetchTweetstorm.js
@@ -1,11 +1,17 @@
 import { BASE_URL } from '../constants';
 
-async function fetchTweetstorm({ inReplyToTweetUrl, items, userId }) {
+async function fetchTweetstorm({ cover, inReplyToTweetUrl, items, userId }) {
+  // CHECK: https://programmingwithmosh.com/javascript/react-file-upload-proper-server-side-nodejs-easy/
+
+  const formData = new FormData();
+
+  formData.append('cover', cover);
+  formData.append('inReplyToTweetUrl', inReplyToTweetUrl);
+  formData.append('items', JSON.stringify(items));
+  formData.append('userId', userId);
+
   const response = await fetch(`${BASE_URL}/api/v2/tweetstorm`, {
-    body: JSON.stringify({ inReplyToTweetUrl, items, userId }),
-    headers: {
-      'Content-Type': 'application/json'
-    },
+    body: formData,
     method: 'POST'
   });
   const json = await response.json();
